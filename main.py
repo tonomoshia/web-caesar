59a32ca2ee7cae6388563942827877ef817e78cf
Tonomoshia <tonomoshia@gmail.com>
2019-09-23 14:31:42 -0400

    added encrypt function but it is not doing anything.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from caesar import rotate_string
 
 app = Flask(__name__)
@@ -10,34 +10,41 @@ form = """
     <html>
         <head>
             <style>
-                form {
+                form {{
                     background-color: #eee;
                     padding: 20px;
                     margin: 0 auto;
                     width: 540px;
                     font: 16px sans-serif;
                     border-radius: 10px;
-                }
-                textarea {
+                }}
+                textarea {{
                     margin: 10px 0;
                     width: 540px;
                     height: 120px;
-                }
+                }}
             </style>
         </head>
         <body>
-           <form action="post">
+           <form action = "/" method="POST">
                 <label for="rot">Rotate by:</label>
                 <input type ="text" name ="rot" id="rot" value="0" />
                 <input type="textarea" name ="text" rows="4" cols="50" />
-                <input type="submit" />
+                <input type="submit" value="Encrypt"/>
             </form>
         </body>
     </html>
 """
 @app.route("/")
 def index():
-    return form
+    return form.format("")
 
+
+@app.route("/", methods=['POST'])
+def encrypt():
+    rot = int(request.form['rot'])
+    message = str(request.form['text'])
+    encrypted_text = rotate_string(message, rot)
+    return form.format(encrypted_text)
 
 app.run()
